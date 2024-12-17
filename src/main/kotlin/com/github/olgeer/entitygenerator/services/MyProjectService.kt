88b0358bb786ab38786b3dc440be40a6c1684a98
GitHub Action <action@github.com>
2024-12-17 03:04:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/olgeer/entitygenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/olgeer/entitygenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.olgeer.entitygenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.olgeer.entitygenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
